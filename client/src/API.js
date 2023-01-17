92530a73bbf2e29165fe0a5bfe6d741811d9ed30
Siddhu517 <siddhusadadekar517@gmail.com>
2023-01-17 18:32:34 +0530

    change 2

--- client/src/API.js
+++ client/src/API.js
@@ -1,39 +1,39 @@
 import axios from "axios";
-
+const url = process.env.REACT_APP_PUBLIC_API;
 export const getData = async () => {
-  return await axios.get(`/all`);
+  return await axios.get(`${url}/all`);
 };
 
 export const getYear = async (endYear) => {
-  return await axios.post(`/year`, { endYear });
+  return await axios.post(`${url}/year`, { endYear });
 };
 
 export const getTopic = async (topic) => {
-  return await axios.post(`/topic`, { topic });
+  return await axios.post(`${url}/topic`, { topic });
 };
 
 export const getSector = async (sector) => {
-  return await axios.post(`/sector`, { sector });
+  return await axios.post(`${url}/sector`, { sector });
 };
 
 export const getRegion = async (region) => {
-  return await axios.post(`/region`, { region });
+  return await axios.post(`${url}/region`, { region });
 };
 
 export const getPEST = async (pest) => {
-  return await axios.post(`/pest`, { pest });
+  return await axios.post(`${url}/pest`, { pest });
 };
 
 export const getSource = async (source) => {
-  return await axios.post(`/source`, { source });
+  return await axios.post(`${url}/source`, { source });
 };
 
 export const getCountry = async (country) => {
-  return await axios.post(`/country`, { country });
+  return await axios.post(`${url}/country`, { country });
 };
 
 export const getALLFilter = async (allFilterData) => {
-  return await axios.post(`/allfilter`, {
+  return await axios.post(`${url}/allfilter`, {
     allFilterData,
   });
 };
--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,7 @@ import "./App.css";
 import Index from "./components/Index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import axios from "axios";
-
 function App() {
-  axios.defaults.baseURL = process.env.REACT_APP_PUBLIC_API;
   return (
     <div className="app">
       <BrowserRouter>
