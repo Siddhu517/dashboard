f65e78527db982f57b96d58fd7c09c90191dc8f3
Siddhu517 <siddhusadadekar517@gmail.com>
2023-02-14 10:43:55 +0530

    remove dynamic import error

--- client/src/components/DisplaySection/BarGraph.js
+++ client/src/components/DisplaySection/BarGraph.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.7.0/+esm";
+//import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.7.0/+esm";
+import * as d3 from "d3";
 
 const BarGraph = ({ filterData, onClick }) => {
   const chartRef = useRef();
